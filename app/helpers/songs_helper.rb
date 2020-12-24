6b848398417088df71e49a294fa1333d104bb304
Sophie Gu <sophieqgu@gmail.com>
2020-12-24 03:52:14 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,9 +1,16 @@
 module SongsHelper
-  def artist_select(song)
-    if song.artist.nil?
-      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-    else
-      hidden_field_tag "song[artist_id]", song.artist_id
+  def artist_select(song, path)
+  if song.artist && path == "nested"
+    hidden_field_tag "song[artist_id]", song.artist_id
+  else
+    select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+  end
+end
+
+# Display name if editing through nested
+  def display_name(song, path)
+    if song.artist && path == "nested"
+      song.artist.name
     end
   end
 end
